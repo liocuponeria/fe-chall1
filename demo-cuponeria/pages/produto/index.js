c99e9cf4b8487ceffd0237973347572395bbf851
Antônio Della Flora <antoniodellaflora@gmail.com>
2021-03-11 13:01:20 -0300

    initial commit

---
+++ demo-cuponeria/pages/produto/index.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function ProductPage() {
+  return <div>
+    <h1>Página do produto</h1>
+    <Link href="/">
+      <a>Back to home</a>
+    </Link>
+  </div>
+}
