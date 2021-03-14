b37c87265701aac8f513d604d8b8490cf9da2bbd
Antônio Della Flora <antoniodellaflora@gmail.com>
2021-03-14 20:58:42 -0300

    feat: :lipstick: Novo arquivo de tema
    Criado um arquivo de tema para centralizar todo o layout da aplicação

---
+++ demo-cuponeria/utils/theme.js
@@ -0,0 +1,17 @@
+export default {
+  productCard: {
+    primary: {
+      background: 'rgba(200, 118, 118, 0.2)',
+      button: '#EE797E',
+    },
+    secondary: {
+      background: 'rgba(63, 127, 203, 0.2)',
+      button: '#619CEB',
+    },
+  },
+
+  SIZE: {
+    tablet: 768,
+    desktop: 1278,
+  },
+};
