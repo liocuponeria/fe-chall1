d297f53974cdbb70dfd45edf9ebfacb8e907f5c2
Nathan Miguel <nathan.miguel@sapienstec.com>
2021-10-20 19:50:36 -0300

    Resetando estilos padroes do body

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import type { AppProps } from 'next/app'
+import { GlobalStyle } from "../styles/global";
+import type { AppProps } from "next/app";
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );
 }
-export default MyApp
+export default MyApp;
