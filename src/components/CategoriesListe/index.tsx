b1832878c741caf59b7815e5c6d9df3e3d9f6f89
Nathan Miguel <nathan.miguel@sapienstec.com>
2021-10-20 19:51:03 -0300

    WIP lista de categorias

---
+++ src/components/CategoriesListe/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import { List } from "./style";
+
+export const CategoriesListe: NextPage = () => {
+  return (
+    <>
+      <List>
+        
+      </List>
+    </>
+  );
+};
---
+++ src/components/CategoriesListe/style.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const List = styled.ul`
+  width: 88.8%;
+
+  display: flex;
+  justify-content: space-between;
+
+  list-style: none;
+`
