aa3a622b0bc79abb8a733863176e2a9f303ae31c
Nathan Miguel <nathan.miguel@sapienstec.com>
2021-10-21 02:02:39 -0300

    Removido import desnecessario

--- src/components/CategoriesList/index.tsx
+++ src/components/CategoriesList/index.tsx
@@ -2,11 +2,6 @@ import { useContext, useMemo } from "react";
 import { CategoriesListContext } from "../../context/categoriesContext";
 import { RadioInput } from "../RadioInput";
 import { Container } from "./style";
-import type { NextPage } from "next";
-
-interface ICategoriesListProps {
-  categoriesData: string[];
-}
 
 export const CategoriesList = () => {
   const { categoriesList } = useContext(CategoriesListContext);
